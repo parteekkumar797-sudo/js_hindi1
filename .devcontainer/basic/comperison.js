2fe57216fd1b3a3f2dd5ac3ae8a2af21f2e8a432
parteek kumar <parteekkumar797@gmail.com>
2026-05-31 14:26:59 +0000

    compair

---
+++ .devcontainer/basic/comperison.js
@@ -0,0 +1,7 @@
+console.log("2"<=1);// compare same data types
+
+console.log(null==0);
+console.log(null<0);
+console.log(null<=0);// compare same data types
+
+//===  this is check values datatypes also 
