0cf80c7270a22134a3183ae37f0cdf53e363df64
parteek kumar <parteekkumar797@gmail.com>
2026-05-31 14:17:28 +0000

    oprations

--- .devcontainer/basic/conversionopration.js
+++ .devcontainer/basic/conversionopration.js
@@ -10,3 +10,21 @@ console.log( valueinscore);// but output is nan(not a number )
 
 // when we convert 1 to boolen we saw output is true 0=faulse
 // empty string=true or string=faulse
+
+
+//******************opration ***************************
+
+// let value=3
+// let negvalue=-value
+// console.log(negvalue);
+
+let str1='p'
+let str2='k'
+
+console.log(str1+str2);//to add string
+
+console.log("1"+2);
+console.log("1"+2);
+console.log("1"+2+2); // change all veriable to string when 1st value is string
+console.log(1+2+'2');  //add numbers whwen last value is string
+console.log("1"+(2+2)); // use a bracesss for this type of code
