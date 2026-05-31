84a5c7397a4d5072fcff01caf09ff3f65dd7d155
parteek kumar <parteekkumar797@gmail.com>
2026-05-31 14:44:24 +0000

    datatypeesummary

---
+++ .devcontainer/basic/datatypes_summary.js
@@ -0,0 +1,33 @@
+// // premitive
+// //types
+// String
+// Number
+// Boolean
+// null
+// undefined
+// BigInt
+let a=23456889n
+console.log(typeof a);
+
+// Symbol  
+const id = Symbol('123')
+const bidid = Symbol('123')
+console.log(id === bidid); // these are also diffrent
+
+// //nonpremitive/ referencetyoe
+
+// Object
+let myobj = {
+    name:'hitesh',
+    age:22,
+}
+// Array
+const heros=['pk','nc']
+// functions
+const myfunction = fun()
+{
+    console.log("helloworld");
+}
+
+
+// java is dynamic or static  
