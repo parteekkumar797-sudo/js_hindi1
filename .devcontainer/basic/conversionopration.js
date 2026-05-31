10032eed2883368d4bf6303dd9a63cfa9b8f60ef
parteek kumar <parteekkumar797@gmail.com>
2026-05-31 13:32:04 +0000

    coversion

---
+++ .devcontainer/basic/conversionopration.js
@@ -0,0 +1,12 @@
+let score='3aa' // this is string value 
+
+console.log(typeof score);
+
+let valueinscore= Number(score) // i change score datatype
+
+console.log( valueinscore);// but output is nan(not a number )
+
+// we can use this to check data type of veriable which came from fronthand
+
+// when we convert 1 to boolen we saw output is true 0=faulse
+// empty string=true or string=faulse
