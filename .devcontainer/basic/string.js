5d73c3feca3203fb9171cd94d23c26c8e29d3ddb
parteek kumar <parteekkumar797@gmail.com>
2026-06-01 12:40:11 +0000

    string

---
+++ .devcontainer/basic/string.js
@@ -0,0 +1,13 @@
+let name="pk";
+let repocount= 20;
+console.log(`i am a ${name} parteek and my repocount is ${repocount}`);
+const myname=new String("pk") 
+console.log(myname.toUpperCase());//to  use the function of string
+// strings ke methods
+
+const newstring = myname.substring(0,4)//print by string keyaddress
+console.log(newstring);
+
+const anotherstring = name.slice(-1,4)//start from - valuues
+console.log(anotherstring);
+//.trim is remove spaces
